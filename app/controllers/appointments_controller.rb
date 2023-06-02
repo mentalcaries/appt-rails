f1180da813c18d97d1071aa4fd89e04a569423aa
Devin Jaggernauth <neexis@gmail.com>
2023-06-02 10:09:02 -0400

    add devise and tailwind

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,12 @@
 class AppointmentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_blog_post, except: [:index, :new, :create]
+
   def index
     @appointments = Appointment.all
   end
 
   def show
-    @appointment = Appointment.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
   end
 
   def new
@@ -22,8 +22,35 @@ class AppointmentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_blog_post
+    @appointment = Appointment.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
   def appointment_params
     params.require(:appointment).permit(:name, :date, :phone, :description)
   end
 
+  def authenticate_user!
+    redirect_to new_user_session_path, alert: "You must sign in to continue" unless user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get "/appointments/new", to: "appointments#new", as: :new_appointment
-  get "/appointments/:id", to: "appointments#show", as: :appointment
-  post "/appointments", to: "appointments#create", as: :appointments
+  resources :appointments
+  # get "/appointments/new", to: "appointments#new", as: :new_appointment
+  # get "/appointments/:id", to: "appointments#show", as: :appointment
+  # patch "/appointments/:id", to: "appointments#update"
+  # delete "/appointments/:id", to: "appointments#destroy"
+  # get "/appointments/:id/edit", to: "appointments#edit", as: :edit_appointment
+  # post "/appointments", to: "appointments#create", as: :appointments
 
   # Defines the root path route ("/")
   root "appointments#index"
