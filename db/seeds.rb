9b02a02465709c2990490bbf2f9063fdc5b48b1e
Devin Jaggernauth <neexis@gmail.com>
2023-06-03 07:29:09 -0400

    update seed password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-users = User.create([{email: "devin@devin.com", password: "encrypted_password"}])
+users = User.create([{email: "devin@devin.com", encrypted_password: "password"}])
